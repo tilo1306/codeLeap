4cca6c690fb6f28ad54ca5e23173b7d09313219c
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-17 13:15:14 -0300

    feat add routes

---
+++ web/routes/router.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import React from "react";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+    },
+
+]);
+
+export default router;
