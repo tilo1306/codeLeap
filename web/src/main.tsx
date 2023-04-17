3975c109143c7ca1becbf65e9ef5557acdcdb823
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-17 13:14:47 -0300

    feat: add theme in styled-components

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import './global.css'
+import { ThemeProvider } from 'styled-components'
+import theme from './theme'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme} >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ web/src/theme/index.ts
@@ -0,0 +1,19 @@
+export default {
+      COLORS: {
+    WHITE: '#FFFFFF',
+
+    DARKGRAY1: "#777777",
+    GRAY: "#DDDDDD",
+    BLUE: "#7695EC"
+
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Roboto',
+  },
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24
+  }
+}
