355572f7bcecb8fcd5de15aac76cf57ddafa8b6d
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-19 11:13:24 -0300

    chore: config alias

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugin: [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@storage': './src/storage',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  }
+}
