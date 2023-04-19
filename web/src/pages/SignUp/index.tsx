bdb8fc8d34bffc19692f5c0a1c785aa64a71203f
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-18 22:30:43 -0300

    feat:  create page signUp andstyles

--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react";
+import { AreaLogin, Container, Title, Text, Input, Buttom, Div } from "./styles";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setName } from '../../redux/reducers/userReducer'
+
 
 export function SignUp() {
+
+    const dispatch = useDispatch()
+    const [username, setUsername] = useState<string>('')
+
+    const navigate = useNavigate();
+
+    const handleSubmit = () => {
+        dispatch(setName(username))
+        localStorage.setItem("username", username);
+
+        navigate('/mainscreen')
+    }
+
     return (
-        <h1>dadjasijdisa</h1>
+        <Container>
+
+            <AreaLogin onSubmit={(e) => {
+                e.preventDefault()
+                handleSubmit()
+            }}>
+                <Title>Welcome to CodeLeap network!</Title>
+                <Text>Please enter your username</Text>
+                <Input required placeholder="John doe" value={username} onChange={({ target }) => setUsername(target.value)} />
+                <Div>
+                    <Buttom disabled={username.length === 0}>Enter</Buttom>
+                </Div>
+            </AreaLogin>
+        </Container>
     )
 }
---
+++ web/src/pages/SignUp/styles.ts
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    align-items: center;
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    width: 100vw;
+`
+export const AreaLogin = styled.form`
+    background: ${({theme})=> theme.COLORS.WHITE};
+    border: 1px solid #CCCCCC;
+    border-radius: 16px;
+    margin: 1.5rem;
+    max-width: 31.25rem;
+    min-height: 12.813rem;
+    padding: 1.5rem;
+`
+export const Title = styled.h1`
+    color: ${({theme})=> theme.COLORS.BLACK};
+    font-size: ${({theme})=> theme.FONT_SIZE.XL}px;
+    font-weight: 700;
+`
+export const Text = styled.p`
+    color: ${({theme})=> theme.COLORS.BLACK};
+    font-size: ${({theme})=> theme.FONT_SIZE.MD}px;
+    margin: 1.5rem 0 0.5rem 0;
+`
+
+export const Input = styled.input`
+    border: 1px solid ${({theme})=> theme.COLORS.DARKGRAY1};   
+    border-radius: 8px;
+    height: 2rem;
+    padding: 0.7rem; 
+    width: 100%;
+`
+export const Div = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 1rem; 
+`
+
+export const Buttom = styled.button`
+    background: ${({theme})=> theme.COLORS.BLUE};
+    border: none;
+    border-radius: 8px;
+    color: ${({theme})=> theme.COLORS.WHITE};
+    height: 2rem;
+    font-size: ${({theme})=> theme.FONT_SIZE.MD}px;
+    font-weight: 700;
+    width: 6.938rem;
+    :enabled{
+        animation: flasher  1s ;
+
+        @keyframes flasher {
+            0% { opacity: 0.3; }
+            50% { opacity: 0.7; }
+            100% { opacity: 1; }
+        }
+    };
+    :hover:enabled {
+        cursor: pointer;
+        transform: scale(1.1);
+        transition: all 0.5s;
+    };
+    :disabled{
+        opacity: 0.3;
+    } 
+  
+`
