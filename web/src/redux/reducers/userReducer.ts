24b1df4808e71a293e3532e93c455394bbc6a310
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-18 22:26:01 -0300

    feat: config redux

---
+++ web/src/redux/reducers/userReducer.ts
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const slice = createSlice({
+    name: 'user',
+    initialState: {
+        name: '',
+    },
+    reducers:{
+        setName: (state, action) => {
+            state.name = action.payload
+        }
+    }
+})
+
+export const {setName} = slice.actions
+export default slice.reducer;
