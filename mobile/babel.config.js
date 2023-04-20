a9f6791b8a8acaf75975f0195396aa483e4af90c
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-20 20:11:32 -0300

    chore: prettier config

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,21 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugin: [
-      'module-resolver',
-      {
-        root: ['./src'],
-        alias: {
-          '@assets': './src/assets',
-          '@components': './src/components',
-          '@routes': './src/routes',
-          '@screens': './src/screens',
-          '@storage': './src/storage',
-          '@theme': './src/theme',
-          '@utils': './src/utils',
-        },
-      },
-    ],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@storage': './src/storage',
+            '@theme': './src/theme',
+            '@utils': './src/utils',
+            '@redux': './src/redux'
+          },
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx'
+          ]
+        }
+      ]
+    ]
   }
 }
